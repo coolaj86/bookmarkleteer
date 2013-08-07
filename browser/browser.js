17b3288d0c6f9b26cf1eae983dad26c9f4df17ae
AJ ONeal <coolaj86@gmail.com>
2013-08-06 19:08:35 -0700

    added hipster lispum bookmarklet

--- browser/browser.js
+++ browser/browser.js
@@ -7,6 +7,8 @@ window.jQuery(function () {
     //, UglifyJS = require('uglify-js')
     , UglifyJS = window.UglifyJS
     , serializeForm = require('serialize-form').serializeFormObject
+    , reEditable = /^#?\/?([^\/]+)\/edit\/([^\/]+)\/?$/
+    , reShareable = /^#?\/?([^\/]+)(?:\/([^\/]+))?\/?$/
     ;
 
   function uglify(code) {
@@ -73,10 +75,38 @@ window.jQuery(function () {
     $('.js-share-container').slideDown();
   }
 
+  function loadEditable() {
+    $('.js-loading-container').show();
+    $('.js-create-container').show();
+  }
+  function loadShareable() {
+    $('.js-loading-container').show();
+    $('.js-product-container').show();
+  }
+  function loadCreateable() {
+    $('.js-create-container').show();
+  }
+
+  $('.js-loading-container').hide();
+  $('.js-create-container').hide();
   $('.js-test-container').hide();
   $('.js-share-container').hide();
   $('.js-bookmarklet-container').hide();
 
   $events.on('submit', 'form.js-script', onSubmit);
   $events.on('click', '.js-share-it', onShareIt);
+
+  // #/xyz123/edit/abc987def345/
+  // #xyz123/edit/abc987def345
+  // TODO add users and allow #xyz123/edit
+  if (reEditable.test(location.hash)) {
+    loadEditable(reEditable.exec(location.hash)[1], reEditable.exec(location.hash)[2]);
+  // #/xyz123/jQuerify/
+  // #xyz123/jQuerify
+  // #xyz123
+  } else if (reShareable.test(location.hash)) {
+    loadShareable(reShareable.exec(location.hash)[1]);
+  } else {
+    loadCreateable();
+  }
 });
